3e88813f7691df06c6d6c35b11be23f6e95134d0
branbot1046 <66091214+branbot1046@users.noreply.github.com>
2021-03-12 16:50:26 -0500

    Add files via upload
    :c

---
+++ singleapp/src/app/Modelo/Persona.ts
@@ -0,0 +1,13 @@
+export class Persona {
+    id: number;
+    nombres: string;
+    apellidos: string;
+    idtipodocumeto: string;
+    documento: number;
+    lugarresidencia: string;
+    fechanacimiento: Date;
+    email: string;
+    telefono: number;
+    usuario: string;
+    password: string;
+}
